2fe982c2b207916a35266d9dd8120f6997617975
gonced8 <gonced8@gmail.com>
2021-02-19 02:20:43 +0000

    landing page

--- website/routes.py
+++ website/routes.py
@@ -1,34 +1,26 @@
-from flask import render_template
+import os
+from flask import render_template, send_from_directory
 from website import app, ext
 
-posts = [
-    {
-        "author": "Gonçalo Raposo",
-        "title": "Primeiro Post",
-        "content": "Isto de fazer sites até é engraçado!",
-        "date_posted": "23 de outubro de 2020"
-    },
-    {
-        "author": "Gonçalo Raposo",
-        "title": "Segundo Post",
-        "content": "Beep boop",
-        "date_posted": "24 de outubro de 2020"
-    }
-]
 
-@app.route('/')
+@app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template("landing_page.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html", title="Sobre")
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"), "favicon.ico", mimetype="icon"
+    )
+
 
 @ext.register_generator
 def index():
-    yield 'home', {}
+    yield "home", {}
+
 
 @ext.register_generator
 def index():
-    yield 'about', {}
+    yield "favicon", {}
